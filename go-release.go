d2ebd0bfd23ce621f64509307162fbd90ea7e725
josh fernandes <joshfernandes@mac.com>
2020-06-10 21:56:29 -0700

    added basic code

--- go-release.go
+++ go-release.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getFilePath(arg string) string {
+	path, err := filepath.Abs(arg)
+	check(err)
+	removeFileName(&path)
+	return path
+}
+
+func removeFileName(path *string) {
+	fileInfo, err := os.Stat(*path)
+	check(err)
+	if fileInfo.Mode()&os.ModeType != os.ModeDir {
+		*path = filepath.Dir(*path)
+	}
+}
+
+func checkExists(path string) {
+	_, err := os.Stat(path)
+	check(err)
+}
 
 func main() {
-	fmt.Println("hello")
+	args := os.Args[1:]
+	path := "."
+	if length := len(args); length != 0 {
+		path = args[0]
+	}
+	origin := getFilePath(path)
+	checkExists(origin)
+	fmt.Println(origin)
 }
